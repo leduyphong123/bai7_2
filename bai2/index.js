c1ed9c24360a20e8c6d47f0ba3a02e6e8b533096
leduyphong123 <leduyphongck789@gmail.com>
2023-04-10 15:35:42 +0700

    xong 1 2 3

---
+++ bai2/index.js
@@ -0,0 +1,5 @@
+let wight=20;
+let height=10;
+let aere= wight*height;
+
+document.write("dien tich = "+aere);
---
+++ bai3/index.js
@@ -0,0 +1,9 @@
+let a=prompt("Nhap a");
+let b=prompt("Nhap b");
+
+if(a%b==0){
+    document.write("a la boi so cua b");
+}
+else{
+    document.write("a k la boi so cua b");
+}
